c0ee847dd43bf164b1da9621aa6f9f6659593046
marciorps <marciorps@yahoo.com,.br>
2023-11-29 15:40:03 -0300

    kwargs

---
+++ kwargs.py
@@ -0,0 +1,8 @@
+def fazer_calculo(nome, *args, **kwargs):
+    print(nome)
+    print(args)
+    print(kwargs)
+    for arg in args:
+        print(arg)
+    for kwarg in kwargs.values():
+        print(kwarg)
